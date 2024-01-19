f500b9b212915dcfea82b832614270fe24a1a369
Anirudh <anirudhphaniraj@gmail.com>
2024-01-19 17:02:07 +0530

    updated website

--- practice.js
+++ practice.js
@@ -1,2 +1,10 @@
 console.log("hello")
 document.getElementById("demo").innerHTML = "hello"
+let car1 = "BMW"
+let car2 = "Audi"
+let car3 = "Porsche"
+const cars = [car1, car2, car3]
+console.log(cars)
+let x=2
+let y=4
+console.log(x+y)
